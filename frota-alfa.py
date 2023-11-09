2e26fb45c5f96c68ff7669ec13d62f9c4195f022
Mariasina <carolbeveer@gmail.com>
2023-11-09 16:26:08 -0300

    frota alfa exercicio

---
+++ frota-alfa.py
@@ -0,0 +1,24 @@
+ano_luz = {
+"pc": 0.31,
+"al": 1,
+"ae": 63241.09,
+"ml": 525960.23,
+"sl": 31557609.92
+}
+
+unidades = ["Parsec (pc)", "Ano-Luz (al)", "Unidade Astronômica (ae)", "Minuto-Luz (ml)", "Segundo-Luz (sl)"]
+
+opcao = 1
+
+for i in unidades:
+    print(i)
+
+valor = float(input("\nValor a ser convertido: "))
+unidade_origem = input("Converter de: ")
+unidade_destino = input("Converter para: ")
+
+if unidade_destino in ano_luz and unidade_origem in ano_luz:
+    conversao = valor / ano_luz[unidade_origem]
+    conversao = conversao * ano_luz[unidade_destino]
+
+    print(f"Conversão: {valor} {unidade_origem} = {conversao} {unidade_destino}")
--- restaurante-boca-feliz.py
+++ restaurante-boca-feliz.py
@@ -1,33 +1,42 @@
-estoque = {'pao': 10, 'hamburguer': 12, 'tomate': 5, 'bacon': 5, 'ovo': 5}
+while True:
+     try:
+          estoque = {'pao': 10, 'hamburguer': 12, 'tomate': 5, 'bacon': 5, 'ovo': 5}
 
-cardapio = {
-'x-burguer': ['pao', 'hamburguer'],
-'x-salada': ['pao', 'hamburguer', 'tomate'],
-'x-bacon': ['pao', 'hamburguer', 'tomate', 'bacon'],
-'x-egg': ['pao', 'hamburguer', 'ovo'],
-'x-tudo': ['pao', 'hamburguer', 'tomate', 'hamburguer', 'bacon', 'ovo']
-}
+          cardapio = {
+          'x-burguer': ['pao', 'hamburguer'],
+          'x-salada': ['pao', 'hamburguer', 'tomate'],
+          'x-bacon': ['pao', 'hamburguer', 'tomate', 'bacon'],
+          'x-egg': ['pao', 'hamburguer', 'ovo'],
+          'x-tudo': ['pao', 'hamburguer', 'tomate', 'hamburguer', 'bacon', 'ovo']
+          }
 
-pedido = {1: "x-burguer", 2: "x-salada", 3: "x-bacon", 4: "x-egg", 5: "x-tudo"}
+          pedido = {1: "x-burguer", 2: "x-salada", 3: "x-bacon", 4: "x-egg", 5: "x-tudo"}
 
-opcao = 1
+          opcao = 1
 
-concluido = True
+          concluido = True
 
-for hamburguers, ingredientes in cardapio.items():
-     print(f"{opcao}. {hamburguers}: {ingredientes}")
-     opcao += 1
 
-while opcao != 0:
-     opcao = int(input("O que deseja pedir (0 para sair)? "))
-     if opcao >= 1 or opcao <= 5:
-         concluido = True
-         for i in cardapio.get(pedido[opcao]):
-               if estoque[i] > 0:
-                    estoque[i] -= 1
-               else:
-                    print(f"Infelizmente acabou o {i}")
-                    concluido = False
-     if concluido == True:
-          print(f"um {pedido[opcao]} saindo no capricho!!!")
 
+          for hamburguers, ingredientes in cardapio.items():
+               print(f"\n\n{opcao}. {hamburguers}: {ingredientes}")
+               opcao += 1
+
+          while opcao != 0:
+               opcao = int(input("O que deseja pedir (0 para sair)? "))
+               if opcao >= 1 or opcao <= 5:
+                    concluido = True
+               for i in cardapio.get(pedido[opcao]):
+                         if estoque[i] > 0:
+                              estoque[i] -= 1
+                         else:
+                              print(f"Infelizmente acabou o {i}")
+                              concluido = False
+               if concluido == True:
+                    print(f"um {pedido[opcao]} saindo no capricho!!!")
+
+     except ValueError:
+          print("\n\nValor inválido!")
+     except KeyboardInterrupt:
+          print("\n\nPrograma terminado com sucesso.")
+          break
